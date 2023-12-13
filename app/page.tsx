d4c3024cd0aa3fb604d23745a339ad6cd57d509b
Chris Ware <c.p.ware@hotmail.com>
2023-12-13 14:15:57 -0700

    About Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Newest from "@/components/Newest/Newest";
 import Hero from "@/components/Hero/Hero";
+import AboutSection from "@/components/AboutSection/AboutSection";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <AboutSection />
       {/* <Newest /> */}
     </div>
   );
---
+++ components/AboutSection/AboutSection.tsx
@@ -0,0 +1,32 @@
+import ContentPadding from "../ContentPadding/ContentPadding";
+import Label from "../Label/Label";
+import LayoutWrapper from "../LayoutWrapper";
+import styles from "./AboutSection.module.css";
+
+const AboutSection = () => {
+  return (
+    <div className={styles.container}>
+      <LayoutWrapper>
+        <ContentPadding>
+          <div className={styles.labelContainer}>
+            <Label color='' text='About us' />
+          </div>
+          <h2 className={styles.heading}>
+            TACO BELL IS A{" "}
+            <span className={styles.span}>FLAVORFUL FEASTING </span>
+            ADVENTURE. INDULGE, SIP, AND ENJOY THE{" "}
+            <span className={styles.span}>TASTY DELIGHTS </span> WITH YOUR
+            COMPANIONS.
+          </h2>
+          <p className={styles.copy}>
+            SPAWNED BY A CREW OF TACO ENTHUSIASTS AIMING TO IGNITE A TASTEBUD
+            REVOLUTION IN Downey, California , TACO BELL AROSE. WE EMBRACE
+            FLAVOR, CHERISH REFRESHMENTS, AND THRIVE ON CRAFTING MEMORABLE
+            MOMENTS OF TACO DELIGHT.
+          </p>
+        </ContentPadding>
+      </LayoutWrapper>
+    </div>
+  );
+};
+export default AboutSection;
